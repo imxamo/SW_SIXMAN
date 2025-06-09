ab3eb0401d19279a552fefb04f84273491f0a04a
teleenql <kang31232@gmail.com>
2025-06-09 20:11:21 +0900

    Update test.ino

--- Arduino/test.ino
+++ Arduino/test.ino
@@ -11,6 +11,7 @@
 #define PUMP_ENA 18
 #define PUMP_PWM_FREQ 5000
 #define PUMP_PWM_RESOLUTION 8
+#define COOLING_FAN_PIN 13
 #define loop_sec 1000
 
 DHT dht(TEMP_PIN, DHTTYPE);
@@ -30,6 +31,9 @@ void setup() {
   // 펌프 제어 핀 설정
   pinMode(PUMP_IN1, OUTPUT);
   pinMode(PUMP_IN2, OUTPUT);
+  
+  // 쿨링팬 핀 설정
+  pinMode(COOLING_FAN_PIN, OUTPUT);
   
   // PWM 초기화 (ESP32 Arduino Core 3.x 버전)
   ledcAttach(PUMP_ENA, PUMP_PWM_FREQ, PUMP_PWM_RESOLUTION);
@@ -66,17 +70,60 @@ void temp() {
   Serial.println("%");
 }
 
-void pump() {
+void pump(int pump_seconds) {
+  if (pump_seconds <= 0) {
+    Serial.println("급수 불필요 - 토양 습도 적정");
+    return;
+  }
+  
+  Serial.print("급수 시작 - ");
+  Serial.print(pump_seconds);
+  Serial.println("초간 급수");
+  
   // 펌프 ON
   digitalWrite(PUMP_IN1, HIGH);
   digitalWrite(PUMP_IN2, LOW);
-  ledcWrite(PUMP_ENA, 255);  // 최대 속도 (핀 번호 직접 사용)
-  delay(5 * loop_sec);
+  ledcWrite(PUMP_ENA, 255);  // 최대 속도
+  delay(pump_seconds * 1000);  // 초 단위로 변환
   
   // 펌프 OFF
   digitalWrite(PUMP_IN1, LOW);
   digitalWrite(PUMP_IN2, LOW);
   ledcWrite(PUMP_ENA, 0);
+  
+  Serial.println("급수 완료");
+}
+
+int get_pump_time(int soil_moisture) {
+  // 토양 습도 센서 값에 따른 급수 시간 결정
+  // 값이 낮을수록 건조함 (센서에 따라 조정 필요)
+  
+  if (soil_moisture < 300) {        // 매우 건조 (20-30% 습도)
+    Serial.println("토양 상태: 매우 건조 - 8초 급수");
+    return 8;
+  } else if (soil_moisture < 400) { // 건조 (30-40% 습도)
+    Serial.println("토양 상태: 건조 - 6초 급수");
+    return 6;
+  } else if (soil_moisture < 500) { // 약간 건조 (40-50% 습도)
+    Serial.println("토양 상태: 약간 건조 - 4초 급수");
+    return 4;
+  } else if (soil_moisture < 600) { // 조금 부족 (50-60% 습도)
+    Serial.println("토양 상태: 조금 부족 - 2초 급수");
+    return 2;
+  } else {                          // 적정 습도 (60% 이상)
+    Serial.println("토양 상태: 적정 습도 - 급수 불필요");
+    return 0;
+  }
+}
+
+void cooling_fan(bool fan_on) {
+  if (fan_on) {
+    digitalWrite(COOLING_FAN_PIN, HIGH);  // 쿨링팬 ON
+    Serial.println("쿨링팬 ON");
+  } else {
+    digitalWrite(COOLING_FAN_PIN, LOW);   // 쿨링팬 OFF
+    Serial.println("쿨링팬 OFF");
+  }
 }
 
 int pump_timer = 3; // 최초 실행시 펌프 작동을 위함
@@ -86,13 +133,22 @@ void loop() {
   water();
   temp();
   
-  if(1) { // 루프 3회마다 1번씩 실행 pump_timer == 3
-    pump();
-    pump_timer = 0;
+  // 온도에 따른 쿨링팬 제어 (30도 이상이면 ON)
+  if (temp_temp >= 30.0 && !isnan(temp_temp)) {
+    cooling_fan(true);
+  } else {
+    cooling_fan(false);
   }
-  else {
+  
+  // 토양 습도에 따른 자동 급수 (3회 루프마다 1번씩 체크)
+  if (pump_timer >= 3) {
+    int pump_time = get_pump_time(dirt_value);
+    pump(pump_time);
+    pump_timer = 0;
+  } else {
     pump_timer++;
   }
   
+  Serial.println("====================");
   delay(5 * loop_sec);  // 5초마다 측정
 }
