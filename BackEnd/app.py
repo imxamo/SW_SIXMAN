7897f35fcc2b1f35b1b3c3c31432b116d7fa4c1a
imxamo <163809900+imxamo@users.noreply.github.com>
2025-09-22 21:23:47 +0900

    Update app.py

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -7,4 +7,4 @@ def hello():
     return "Hello from school server!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)  # 외부 공개
+    app.run(host="116.124.191.174", port="15020")  # 외부 공개
