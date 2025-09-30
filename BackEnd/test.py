05f8e11d868366caf9a073a8e67ee4eedbf3ebf5
teleenql <kang31232@gmail.com>
2025-09-30 21:18:48 +0900

    Update test.py

--- BackEnd/test.py
+++ BackEnd/test.py
@@ -1,6 +1,7 @@
 import sqlite3
+
 conn = sqlite3.connect('cam_server.db')
-conn.execute("DELETE FROM uploads WHERE file_path LIKE '%20250923_180549.txt%'")
+conn.execute("DELETE FROM uploads")  # 모든 행 삭제
 conn.commit()
-print("삭제 완료!")
+print("전체 데이터 삭제 완료!")
 conn.close()
