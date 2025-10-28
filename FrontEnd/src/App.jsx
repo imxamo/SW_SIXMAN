c3cd6f2ea9850241c6dbc2ce4abc923d9ce2549b
teleenql <kang31232@gmail.com>
2025-10-28 19:10:25 +0900

    Update App.jsx

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -122,6 +122,8 @@ function App() {
               <li>습도: {sensorData?.humidity} %</li>
               <li>토양 수분: {sensorData?.soil_moisture}</li>
               <li>수위: {sensorData?.water_level} %</li>
+              <li>쿨링팬 상태: {sensorData?.cooling_fan}</li>
+              <li>LED 상태: {sensorData?.led}</li>
               <li>⏱ {sensorData?.timestamp}</li>
             </ul>
           ) : (
